ec5f6dfaf112ccee540a2cd43db884e94eb72515
Krutarth742 <krutarth.z@tridhyatech.com>
2024-04-09 10:58:08 +0530

    First Commit of Angular

---
+++ src/app/components/employee-list/employee-list.component.ts
@@ -0,0 +1,50 @@
+import { Component, inject } from '@angular/core';
+import { IEmployee } from '../../interfaces/employee';
+import { HttpService } from '../../http.service';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-employee-list',
+  standalone: true,
+  imports: [MatTableModule, MatButtonModule, RouterLink],
+  templateUrl: './employee-list.component.html',
+  styleUrl: './employee-list.component.css',
+})
+export class EmployeeListComponent {
+  employeeList: IEmployee[] = [];
+  httpService = inject(HttpService);
+  router = inject(Router);
+  
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'email',
+    'age',
+    'phone',
+    'salary',
+    'action'
+  ];
+  ngOnInit() {
+    this.getEmployeesFromServer();
+  }
+
+  getEmployeesFromServer() {
+    this.httpService.getAllEmployees().subscribe((result) => {
+      this.employeeList = result;
+      console.log(this.employeeList);
+    });
+  }
+
+  edit(id: number) {
+    console.log(id);
+    this.router.navigateByUrl("/employee/" + id);
+  }
+
+  delete(id: number) {
+    this.httpService.deleteEmployee(id).subscribe(() => {
+      // this.employeeList = this.employeeList.filter(x => x.id! = id);
+      this.getEmployeesFromServer();
+    })
+  }
+}
